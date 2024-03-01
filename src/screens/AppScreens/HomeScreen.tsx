25502f4ed3683962ea4cdeef0597d2691308aa18
Vijay R <vijay.r@experionglobal.local>
2024-03-01 11:45:45 +0530

    cardComponent with old homescreen

--- src/screens/AppScreens/HomeScreen.tsx
+++ src/screens/AppScreens/HomeScreen.tsx
@@ -1,27 +1,25 @@
 import React from 'react';
-// import colors from '../../utils/colorPallete';
-import { View } from 'react-native';
-import CardComponent from '../../componets/cardComponent';
+import colors from '../../utils/colorPallete';
+import { StyleSheet, Text, View } from 'react-native';
 
 const HomeScreen = () => {
   return (
-    <View>
-      {/* <Text style={styles.text}>Home Screen</Text> */}
-      <CardComponent alignToSides ={false}/>
+    <View style={styles.container}>
+      <Text style={styles.text}>Home Screen</Text>
     </View>
   );
 };
 
-// const styles = StyleSheet.create({
-//   container: {
-//     backgroundColor: colors['secondary-light'],
-//     color: colors['primary-text'],
-//     flex: 1,
-//   },
-//   text: {
-//     color: colors['primary-text'],
-//     fontSize: 40,
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors['secondary-light'],
+    color: colors['primary-text'],
+    flex: 1,
+  },
+  text: {
+    color: colors['primary-text'],
+    fontSize: 40,
+  },
+});
 
 export default HomeScreen;
