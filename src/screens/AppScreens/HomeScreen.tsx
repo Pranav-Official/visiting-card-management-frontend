a4b1dd82960da77e2d56b33d50fa154fd265e847
Vijay R <vijay.r@experionglobal.local>
2024-03-01 12:58:52 +0530

    feat:card component completed with button

--- src/screens/AppScreens/HomeScreen.tsx
+++ src/screens/AppScreens/HomeScreen.tsx
@@ -1,27 +1,25 @@
 import React from 'react';
 import colors from '../../utils/colorPallete';
 import { StyleSheet, Text, View } from 'react-native';
-import CardComponent from '../../componets/cardComponent';
 
 const HomeScreen = () => {
   return (
-    <View>
-      {/* <Text style={styles.text}>Home Screen</Text> */}
-      <CardComponent alignToSides = {true}></CardComponent>
+    <View style = {styles.container}>
+      <Text style={styles.text}>Home Screen</Text>
     </View>
   );
 };
 
-// const styles = StyleSheet.create({
-//   container: {
-//     backgroundColor: colors['secondary-light'],
-//     color: colors['primary-text'],
-//     flex: 1,
-//   },
-//   text: {
-//     color: colors['primary-text'],
-//     fontSize: 40,
-//   },
-// });
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: colors['secondary-light'],
+    color: colors['primary-text'],
+    flex: 1,
+  },
+  text: {
+    color: colors['primary-text'],
+    fontSize: 40,
+  },
+});
 
 export default HomeScreen;
