c6723f745f4e2cf497aba2becabe4940a8680e7b
Vijaysaintgits <vijayr2906@gmail.com>
2024-03-04 18:00:26 +0530

    feat:cardComponent

--- src/components/CardComponent.tsx
+++ src/components/CardComponent.tsx
@@ -1,38 +1,40 @@
+//Card Component 
+
 import React from 'react';
-import { View, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import {View, Text, StyleSheet, TouchableOpacity} from 'react-native';
 import PersonIcon from '../assets/images/person.svg';
 import PhoneIcon from '../assets/images/phone.svg';
 import MailIcon from '../assets/images/mail.svg';
 import CompanyIcon from '../assets/images/organisation.svg';
 import ArrowIcon from '../assets/images/Arrow.svg';
 import colors from '../utils/colorPallete';
-
+ 
 const styles = StyleSheet.create({
   card_container: {
     height: 170,
-    paddingHorizontal: 30,
+    paddingRight: 20,
     backgroundColor: colors['secondary-light'],
     borderRadius: 10,
     paddingTop: 10,
     shadowColor: '#000',
-    shadowOffset: { width: -1, height: 4 },
+    shadowOffset: {width: -1, height: 4},
     shadowOpacity: 0.4,
-    shadowRadius: 4, 
-    elevation: 5, 
+    shadowRadius: 4,
+    elevation: 5,
   },
   first_row: {
     flexDirection: 'row',
     flex: 2,
-    paddingTop:10,
-    paddingLeft:20,
+    paddingTop: 10,
+    paddingLeft: 20,
   },
   second_row: {
     flexDirection: 'row',
     flex: 2,
     justifyContent: 'center',
     paddingLeft: 20,
-    marginBottom:10,
-    gap:10,
+    marginBottom: 10,
+    gap: 10,
   },
   third_row: {
     flexDirection: 'row',
@@ -40,8 +42,7 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
     paddingLeft: 20,
     gap: 10,
-    marginBottom:10,
-
+    marginBottom: 10,
   },
   card_name: {
     fontFamily: 'Roboto',
@@ -51,54 +52,61 @@ const styles = StyleSheet.create({
   sr1: {
     flexDirection: 'row',
     flex: 1,
-    backgroundColor:colors['accent-white'],
-    height:33,
-    alignItems:'center',
-    
+    backgroundColor: colors['accent-white'],
+    height: 33,
+    alignItems: 'center',
+    paddingLeft: 10,
   },
   job_position: {
     fontFamily: 'Roboto',
     fontSize: 12,
     marginRight: 40,
-    color: '102D4A',
+    color: '#102D4A',
     marginLeft: 10,
+    width: 110,
   },
   sr2: {
     flexDirection: 'row',
     flex: 1,
-    backgroundColor:colors['accent-white'],
-    height:33,
-    alignItems:'center',
+    backgroundColor: colors['accent-white'],
+    height: 33,
+    alignItems: 'center',
+    paddingLeft: 10,
   },
   tr1: {
     flexDirection: 'row',
     flex: 1,
-    backgroundColor:colors['accent-white'],
-    height:33,
-    alignItems:'center',
+    backgroundColor: colors['accent-white'],
+    height: 33,
+    alignItems: 'center',
+    paddingLeft: 10,
   },
   tr2: {
     flexDirection: 'row',
     flex: 1,
-    backgroundColor:colors['accent-white'],
-    height:33,
-    alignItems:'center',
+    backgroundColor: colors['accent-white'],
+    height: 33,
+    alignItems: 'center',
+    paddingLeft: 10,
   },
   phone_number: {
     fontFamily: 'Roboto',
     fontSize: 12,
     marginLeft: 10,
+    width: 110,
   },
   mail_text: {
     fontSize: 12,
     fontFamily: 'Roboto',
     marginRight: 40,
     marginLeft: 10,
+    width: 110,
   },
   company_name: {
     fontSize: 12,
     fontFamily: 'Roboto',
     marginLeft: 10,
+    width: 110,
   },
   card_button: {
     backgroundColor: colors['primary-accent'],
@@ -118,16 +126,17 @@ const styles = StyleSheet.create({
     marginRight: 0,
   },
 });
-
+ 
 interface Prop {
   name: string;
   job_position: string;
   email: string;
   phone_number: string;
   company_name: string;
-  alignToSides: boolean;
+  alignToSides?: boolean;
+  clickFunc?: () => void;
 }
-
+ 
 // type Card = {card_name:string,job_role:string,email:string,company_name:string}
 const CardComponent: React.FC<Prop> = ({
   alignToSides,
@@ -136,26 +145,26 @@ const CardComponent: React.FC<Prop> = ({
   email,
   phone_number,
   company_name,
+  clickFunc,
 }): JSX.Element => {
   return (
-    <View style={styles.card_container}>
+    <TouchableOpacity style={styles.card_container} onPress={clickFunc}>
       <View
         style={[
           styles.first_row,
           alignToSides
-            ? { justifyContent: 'space-between' }
-            : { justifyContent: 'center' },
-        ]}
-      >
+            ? {justifyContent: 'space-between'}
+            : {justifyContent: 'center'},
+        ]}>
         <Text style={styles.card_name}>{name}</Text>
         {alignToSides && (
-          <TouchableOpacity style={styles.card_button}>
+          <TouchableOpacity style={styles.card_button} onPress={clickFunc}>
             <Text style={styles.button_text}>View Card</Text>
             <ArrowIcon width={10} height={10} fill={'black'}></ArrowIcon>
           </TouchableOpacity>
         )}
       </View>
-
+ 
       <View style={styles.second_row}>
         <View style={styles.sr1}>
           <PersonIcon width={14} height={14} fill={'black'} />
@@ -176,8 +185,9 @@ const CardComponent: React.FC<Prop> = ({
           <Text style={styles.company_name}>{company_name}</Text>
         </View>
       </View>
-    </View>
+    </TouchableOpacity>
   );
 };
-
+ 
 export default CardComponent;
+
