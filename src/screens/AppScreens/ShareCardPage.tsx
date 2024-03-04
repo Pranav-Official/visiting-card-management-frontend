205243a98b899f758f4fef0f060db7f4d95d5e0d
Vijaysaintgits <vijayr2906@gmail.com>
2024-03-04 23:15:31 +0530

    feat:Share Card Page started

---
+++ src/screens/AppScreens/ShareCardPage.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import colors from '../../utils/colorPallete';
+import {View,Text} from 'react-native';
+
+<View style  = {styles.main_container}>
+    <View style = {StyleSheet.search_bar-container}>
+
+    </View>
+    
+
+</View>
