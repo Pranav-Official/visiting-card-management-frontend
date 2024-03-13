cc5137753e4db982f0264baba494be28370e2ea5
Vijaysaintgits <vijayr2906@gmail.com>
2024-03-13 21:28:31 +0530

    comment removed

--- src/components/ContactListComponent.tsx
+++ src/components/ContactListComponent.tsx
@@ -3,20 +3,7 @@ import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import colors from '../utils/colorPallete';
 import nameToColor from '../hooks/nameToHex';
 type Contact = { contactName: string; onPress?: () => void };
-// const ContactListComponent = ({ contactName, onPress }: Contact) => (
-//   <TouchableOpacity onPress={onPress}>
-//     <View style={styles.container}>
-//       <View
-//         style={[styles.circle, { backgroundColor: nameToColor(contactName) }]}
-//       >
-//         <Text style={styles.alphabet}>{contactName[0]}</Text>
-//       </View>
-//       <View>
-//         <Text style={styles.nameText}>{contactName}</Text>
-//       </View>
-//     </View>
-//   </TouchableOpacity>
-// );
+
 
 const ContactListComponent = ({ contactName, onPress }: Contact) => {
   // Function to truncate the contact name to a maximum of 14 characters
