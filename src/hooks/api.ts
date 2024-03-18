b9700f36dc7350bb4e93b0fee1d1e22bf0588803
Vipin K P <58673683+VipinVIP@users.noreply.github.com>
2024-03-18 18:11:40 +0530

    use axios interceptors instead of manually passing with each request

--- src/hooks/api.ts
+++ src/hooks/api.ts
@@ -1,9 +1,22 @@
 import axios from 'axios';
+import { getLocalItem } from '../utils/Utils';
+import Constants from '../utils/Constants';
 
 const BASE_URL = process.env.BASE_URL;
 const api = axios.create();
 
 api.defaults.baseURL = BASE_URL;
 api.defaults.headers.common['Content-Type'] = 'application/json';
+
+api.interceptors.request.use(
+  async function (config) {
+    const jwtToken = (await getLocalItem(Constants.USER_JWT)) ?? '';
+    config.headers.Authorization = `Bearer ${jwtToken}`;
+    return config;
+  },
+  function (error) {
+    return Promise.reject(error);
+  },
+);
 
 export default api;
